25e048dda8a1e49f357ea521b777c2cf718c4db4
Andre525 <ghost5run@yandex.ru>
2021-04-01 14:23:12 +0700

    Change Dif

--- Dif/main.c
+++ Dif/main.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const double eps = 0.0001;
+
 double function(double x, double y)
 {
     return pow(x, 2) - 2 * y;
 }
 
-void dif_Eiler(double a, double b, int n, double y0)
+double dif_Eiler(double a, double b, int n, double y0)
 {
     double *y, *x, *func;
     double h;
@@ -24,13 +26,29 @@ void dif_Eiler(double a, double b, int n, double y0)
         y[i] = y[i - 1] + h * func[i - 1];
         func[i] = function(x[i], y[i]);
     }
-    printf("\n\nSolution of the differential equation by the Eiler method:\n\n");
-    for (int i = 0; i < n; i++) {
-        printf("x[%d] = %.1lf  y[%d] = %lf  func[%d] = %lf  hfunc[%d] = %lf\n", i, x[i], i, y[i], i, func[i], i, h * func[i]);
-    }
+    // printf("\n\nSolution of the differential equation by the Eiler method:\n\n");
+    // for (int i = 0; i < n; i++) {
+    //     printf("x[%d] = %.1lf  y[%d] = %lf  func[%d] = %lf  hfunc[%d] = %lf\n", i, x[i], i, y[i], i, func[i], i, h * func[i]);
+    // }
+    return y[n - 1];
 }
 
-void dif_Runge_Kutta(double a, double b, int n, double y0)
+void Per_Eiler(double a, double b, int n, double y0)
+{
+    int n0 = n, k;
+    double sq[2], delta = 1;
+    for (k = 0; delta > eps; n *= 2, k ^= 1) {
+
+        sq[k] = dif_Eiler(a, b, n, y0);
+        if (n > n0) {
+            delta = fabs(sq[k] - sq[k ^ 1]);
+        }
+    }
+    printf("Eiler sq[%d] = %lf\n", k, sq[k]);
+    // return sq[k ^ 1];
+}
+
+double dif_Runge_Kutta(double a, double b, int n, double y0)
 {
     double *y, *x, *k1, *k2, *k3, *k4, *delta_y;
     double h;
@@ -53,15 +71,32 @@ void dif_Runge_Kutta(double a, double b, int n, double y0)
         delta_y[i] = (h / 6) * (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]);
         y[i + 1] = y[i] + delta_y[i];
     }
-    printf("\n\nSolution of the differential equation by the Runge-Kutta method:\n\n");
-    for (int i = 0; i < n; i++) {
-        printf("x[%d] = %.1lf  y[%d] = %lf  k1[%d] = %lf  k2[%d] = %lf  k3[%d] = %lf  k4[%d] = %lf  delta_y[%d] = %lf\n", i, x[i], i, y[i], i, k1[i], i, k2[i], i, k3[i], i, k4[i], i, delta_y[i]);
+    // printf("\n\nSolution of the differential equation by the Runge-Kutta method:\n\n");
+    // for (int i = 0; i < n; i++) {
+    //     printf("x[%d] = %.1lf  y[%d] = %lf  k1[%d] = %lf  k2[%d] = %lf  k3[%d] = %lf  k4[%d] = %lf  delta_y[%d] = %lf\n", i, x[i], i, y[i], i, k1[i], i, k2[i], i, k3[i], i, k4[i], i, delta_y[i]);
+    // }
+    return y[n - 1];
+}
+
+void Per_Runge_Kutta(double a, double b, int n, double y0)
+{
+    int n0 = n, k;
+    double sq[2], delta = 1;
+    for (k = 0; delta > eps; n *= 2, k ^= 1) {
+
+        sq[k] = dif_Runge_Kutta(a, b, n, y0);
+        if (n > n0) {
+            delta = fabs(sq[k] - sq[k ^ 1]) / 3.0;
+        }
     }
+    printf("Runge Kutta sq[%d] = %lf\n", k, sq[k]);
 }
 
 int main()
 {
-    dif_Eiler(0.0, 1.0, 10, 1);
-    dif_Runge_Kutta(0.0, 1.0, 10, 1);
+    // dif_Eiler(0.0, 1.0, 10, 1);
+    Per_Eiler(0.0, 1.0, 10, 1);
+    // dif_Runge_Kutta(0.0, 1.0, 10, 1);
+    Per_Runge_Kutta(0.0, 1.0, 10, 1);
     return 0;
 }
